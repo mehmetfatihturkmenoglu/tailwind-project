7693417d487e8ac9560b8a4663daf050d5cff071
mehmetfatihturkmenoglu <mehmet.fatih.turkmenoglu@hotmail.com>
2023-11-02 10:34:23 +0300

    tailwind project starter files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  //tailwindcss e ait olan classları yazacağımız dosyaları adresler
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
