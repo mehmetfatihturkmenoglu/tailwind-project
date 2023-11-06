7d0375b75ebf0c91e50a90127a67412a5fde9d03
mehmetfatihturkmenoglu <mehmet.fatih.turkmenoglu@hotmail.com>
2023-11-06 17:06:00 +0300

    tailwind project header changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  //tailwindcss e ait olan classları yazacağımız dosyaları adresler
+  // Tailwindcss e ait olan classları yazacağımız dosyaları adresler
   content: ["./public/**/*.{html,js}"],
   theme: {
-    extend: {},
+    // Kullanacağımız Fontfamily özelliklerini extend objesinin içerisine yazacağız, temaya ek bir özellik vermiş olduk
+    extend: {
+      fontFamily: {
+        gemunu: ["Gemunu Libre", "sans-serif"],
+        open: ["Open Sans", "sans-serif"],
+      },
+
+      colors: {
+        "tail-red": "#BC1A45",
+        "tail-melon": "#FFD369",
+        "tail-grey": "#DDDDDD",
+        "tail-white": "#F7F7F7",
+      },
+      // Container isimli bir class tanımladığımız durumda defaultta oluşacak stil
+      container: {
+        center: true,
+        screens: {
+          lg: "1140px",
+          xl: "1140px",
+          "2xl": "1140px",
+        },
+      },
+    },
   },
   plugins: [],
 };
